d65b86b72e59ed93f17cd6ba13bb34947cc66a5a
TEMURBEK <yusupboyevtemur473@gmail.com>
2026-03-05 14:53:50 +0500

    Add files via upload

---
+++ work_18.py
@@ -0,0 +1,87 @@
+#14-misol
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#kichik = min(a, b, c)
+#katta = max(a, b, c)
+#print(kichik, katta)
+
+# if15
+#print("\nif15 - Uchta son kiriting")
+#a=int(input())
+#b=int(input())
+#c=int(input())
+
+#s1=a+b
+#s2=a+c
+#s3=b+c
+
+#if s1>=s2 and s1>=s3:
+#    print(a,b)
+#elif s2>=s1 and s2>=s3:
+#    print(a,c)
+#else:
+#    print(b,c)
+
+# if16
+#print("\nif16 - A B C kiriting")
+#a=float(input())
+#b=float(input())
+#c=float(input())
+
+#if a<b<c:
+#    a*=2
+#    b*=2
+#    c*=2
+#else:
+#    a=-a
+#    b=-b
+#    c=-c
+
+#print(a,b,c)
+
+# if17
+#print("\nif17 - A B C kiriting")
+#a=float(input())
+#b=float(input())
+#c=float(input())
+
+#if (a<b<c) or (a>b>c):
+#    a*=2
+#    b*=2
+#    c*=2
+#else:
+#    a=-a
+#    b=-b
+#    c=-c
+
+#print(a,b,c)
+
+# if18
+#print("\nif18 - Uchta son kiriting")
+#a=int(input())
+#b=int(input())
+#c=int(input())
+
+#if a==b:
+#    print("3")
+#elif a==c:
+ #   print("2")
+#else:
+ #   print("1")
+
+# if19
+#print("\nif19 - To'rtta son kiriting")
+#a=int(input())
+#b=int(input())
+#c=int(input())
+#d=int(input())
+
+#if a==b==c:
+ #   print("4")
+#elif a==b==d:
+#    print("3")
+#elif a==c==d:
+#    print("2")
+#else:
+ #   print("1")
